5ea0efd5852c27abb1086ca20f3cf27d8a7fe298
Expedition Works <100822209+expeditionworks@users.noreply.github.com>
2025-03-27 11:10:15 -0400

    Update movePlayer.php

--- engine/modules/movePlayer.php
+++ engine/modules/movePlayer.php
@@ -1,19 +1,19 @@
 <?php
 // movePlayer.php
 
-function movePlayer($player_id, $miles_traveled, $conn) {
-    // Get player state
-    $playerRow = getPlayerState($player_id, $conn);
-
+function movePlayer($player_id, $miles_traveled, $playerRow) {
     // Update the miles traveled
     $playerRow['player_state']['mile'] += $miles_traveled;
+
+    // Increment the day by 1 (each turn is a day)
+    $playerRow['player_state']['day'] += 1;
 
     // Check if player reached a milestone (you could add more logic here for actual milestones)
     if ($playerRow['player_state']['mile'] >= 100) {
         $playerRow['player_state']['log'][] = ['notes' => 'You reached a new milestone!'];
     }
 
-    // Save the updated player state
-    updatePlayerState($player_id, $playerRow['player_state'], $conn);
+    // Return the updated playerRow
+    return $playerRow;
 }
 ?>
