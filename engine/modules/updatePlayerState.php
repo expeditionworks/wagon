1dc9d5eccd1b31ee51d15ff9c216d5709f5ef0ab
Expedition Works <100822209+expeditionworks@users.noreply.github.com>
2025-03-27 11:51:14 -0400

    Update updatePlayerState.php

--- engine/modules/updatePlayerState.php
+++ engine/modules/updatePlayerState.php
@@ -3,6 +3,9 @@
 
 function updatePlayerState($player_id, $playerState, $conn) {
     // Prepare the updated player state for storage
+    $inventoryJson = json_encode($playerState['inventory']);  // Assign json_encode() result to a variable
+    $logJson = json_encode($playerState['log']);  // Assign json_encode() result to a variable
+
     $query = "UPDATE player_state SET 
               day = ?, mile = ?, morale = ?, inventory = ?, log = ? 
               WHERE player_id = ?";
@@ -13,8 +16,8 @@ function updatePlayerState($player_id, $playerState, $conn) {
         $playerState['day'], 
         $playerState['mile'], 
         $playerState['morale'], 
-        json_encode($playerState['inventory']), 
-        json_encode($playerState['log']),
+        $inventoryJson,  // Pass the variable here
+        $logJson,        // Pass the variable here
         $player_id
     );
 
