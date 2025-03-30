47275356554a9472bb5c3d807b5227fd9509e2d1
Expedition Works <100822209+expeditionworks@users.noreply.github.com>
2025-03-29 23:58:50 -0400

    Update db_connection.php

--- test/db_connection.php
+++ test/db_connection.php
@@ -1,10 +1,8 @@
 <?php
-// db_connection.php
-
 // Database connection setup
 $servername = "localhost";
 $username = "root";  // Default username in MAMP
-$password = "root";      // Default password for MAMP is usually empty
+$password = "root";  // Default password for MAMP is usually empty
 $dbname = "conestoga_wagon";  // Replace with your database name
 
 // Create connection
@@ -13,5 +11,14 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+}
+
+// Connection was successful
+// You can now use $conn for your database operations
+
+// (Remember to close the connection when done)
+function closeConnection() {
+    global $conn;
+    $conn->close();
 }
 ?>
