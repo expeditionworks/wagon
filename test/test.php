96df76023214b790cdcf2c3cca7082ef4c69f799
Expedition Works <100822209+expeditionworks@users.noreply.github.com>
2025-03-31 11:22:35 -0400

    Update test.php

--- test/test.php
+++ test/test.php
@@ -77,6 +77,10 @@ if (isset($updatedPlayerState['weatherThisTurn'])) {
   
     echo "<ul>";    
     echo "<li>Food left: " . $updatedPlayerState['inventory']['Food']['quantity'] . "</li>";
+    echo "<li>Food remaining: " . $updatedPlayerState['inventory']['Food']['quantity'] . " lbs</li>";
+    } else {
+        echo "<li>No food in inventory.</li>";
+    }
     echo "<li>Ammunition: " . $updatedPlayerState['inventory']['Ammunition']['quantity'] . "</li>";
     echo "</ul>";
 
